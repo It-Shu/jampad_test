088898cd09a047cf2afaae6f597678e6f5cbca5f
It-Shu <it.shustov@gmail.com>
2022-02-15 23:19:19 +0300

    auth page styles changed

--- src/Pages/Authorization/Welcome/Welcome.tsx
+++ src/Pages/Authorization/Welcome/Welcome.tsx
@@ -6,8 +6,9 @@ import {Button} from "../../../UI/Button/Button";
 export const Welcome = () => {
     return (
         <div className={s.welcome_block}>
+
             <p className={s.welcome}>Welcome!</p>
-            <p className={s.jampad_text}> <b>JamPad</b> is a service<br/>for automating the evaluation <br/> of employees and candidates!</p>
+            <p className={s.jamPad_text}> <b>JamPad</b> is a service<br/>for automating the evaluation <br/> of employees and candidates!</p>
             <p className={s.sing_up_text}>If you are not part of our community,<br/> feel free to sign up</p>
             <Button buttonName={'Join'} className={s.joinButton}/>
         </div>
