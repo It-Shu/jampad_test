096fd7129cc20a7268429f7912cb7e3d7fa5fb44
It-Shu <it.shustov@gmail.com>
2022-02-16 01:02:06 +0300

    added Vacancies.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import './App.css';
 import {Welcome} from "./Pages/Authorization/Welcome/Welcome";
 import {Auth} from "./Pages/Authorization/Auth";
 import s from './App.module.scss'
+import {Vacancies} from "./Pages/Vacancies/Vacancies";
 
 function App() {
   return (
     <div className={s.app}>
+
         <Welcome/>
         <div className={s.auth}>
             <Auth/>
         </div>
-
+<div><Vacancies/></div>
     </div>
   );
 }
