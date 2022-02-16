f21e4bb001bf8b642356298697570fa2c625d4d8
It-Shu <it.shustov@gmail.com>
2022-02-16 22:08:00 +0300

    create authReducer

--- src/store/reducers/auth-reducer.ts
+++ src/store/reducers/auth-reducer.ts
@@ -1,11 +1,35 @@
 
 
 
-
-type ActionType = {
-
+enum AUTH_ACTIONS_TYPES {
+    IS_LOGGED_IN = 'AUTH/IS_LOGGED_IN'
 }
 
-export const authReducer = (state: any, action: ActionType) => {
+type AuthActionType =
+    ReturnType<typeof isLoggedIn>
 
+type AuthInitialState = {
+    isLogin: boolean
 }
+
+const InitialState: AuthInitialState = {
+    isLogin: false
+}
+
+
+
+export const authReducer = (state = InitialState, action: AuthActionType) :AuthInitialState => {
+switch (action.type) {
+    case AUTH_ACTIONS_TYPES.IS_LOGGED_IN: {
+        return {...state, isLogin: action.payload.status}
+    }
+}
+}
+
+
+export const isLoggedIn = (status: boolean) => ({
+    type: AUTH_ACTIONS_TYPES.IS_LOGGED_IN,
+    payload: {status}
+} as const)
+
+
