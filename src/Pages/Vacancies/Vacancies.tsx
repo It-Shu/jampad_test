57a871107e35323049a44bd0dd8729b73ab3fe70
It-Shu <it.shustov@gmail.com>
2022-02-17 01:23:35 +0300

    added Vacancy block style

--- src/Pages/Vacancies/Vacancies.tsx
+++ src/Pages/Vacancies/Vacancies.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import s from './Vacancies.module.scss'
 
 export const Vacancies = () => {
+
+
     return (
         <div className={s.vacanciesPage}>
             <div className={s.header}>
@@ -61,27 +63,73 @@ export const Vacancies = () => {
                     <p className={s.smallTitle}>Vacancy:</p>
                     <p className={s.title}>System Administrator</p>
                     <div className={s.vacancyEdit}> {/* need to change class Name*/}
-                            <div className={s.vacancyEditBlock}>
-                                <div className={s.vacancyEditSlide}>
-                                    <div>
-                                        <div className={s.smallTitle}>Vacancy</div>
-                                        <div className={s.smallTitle}>Open</div>
+                        <div className={s.vacancyEditBlock}>
+                            <div className={s.vacancyEditSlide}>
+                                <div>
+                                    <div className={s.smallTitle}>Vacancy</div>
+                                    <div className={s.smallTitle}>Open</div>
 
-                                    </div>
-                                    <div className={s.smallTitle}>-</div>
-                                    <div>
-                                        <div className={s.smallTitle}>Step1</div>
-                                        <div className={s.smallTitle}>Resume Screening</div>
-                                    </div>
                                 </div>
-                                <div className={s.dots}> . . . <b>.</b> . . </div>
+                                <div className={s.smallTitle}>-</div>
+                                <div>
+                                    <div className={s.smallTitle}>Step1</div>
+                                    <div className={s.smallTitle}>Resume Screening</div>
+                                </div>
                             </div>
-                            <div className={s.vacancyButtons}>
-                                <button className={s.editButton}>Edit funnel</button>
-                                <button className={s.downloadButton}>Download report</button>
-                            </div>
+                            <div className={s.dots}> . . . <b>.</b> . .</div>
+                        </div>
+                        <div className={s.vacancyButtons}>
+                            <button className={s.editButton}>Edit funnel</button>
+                            <button className={s.downloadButton}>Download report</button>
+                        </div>
                     </div>
-                    <div>Open Vacancies</div>
+                    <div className={s.openVacancies}>
+                        <div className={s.openVacanciesHeader}>
+                            <p className={s.title}>Open Vacancies</p>
+                            <input className={s.input} type="search" placeholder={'search'}/>
+                        </div>
+                        <div className={s.openVacanciesBlocks}>
+                            <div className={s.openVacanciesBlock}>
+                                <div className={s.arrow}>
+                                    {`<`}
+                                </div>
+                                <div>
+                                    <div>General Manager</div>
+                                    <div className={s.smallTitle}>Step 1: Screening resume</div>
+                                </div>
+                            </div>
+                            <div className={s.openVacanciesBlock}>
+                                <div className={s.arrow}>
+                                    {`<`}
+                                </div>
+                                <div>
+                                    <div>System Administrator</div>
+                                    <div className={s.smallTitle}>Step 4: Soft Skills</div>
+                                </div>
+                            </div>
+                            <div className={s.openVacanciesBlock}>
+                                <div className={s.arrow}>
+                                    {`<`}
+                                </div>
+                                <div>
+                                    <div>General Manager</div>
+                                    <div className={s.smallTitle}>Step 1: Screening resume</div>
+                                </div>
+                            </div>
+                            <div className={s.openVacanciesBlock}>
+                                <div className={s.arrow}>
+                                    {`<`}
+                                </div>
+                                <div>
+                                    <div>General Manager</div>
+                                    <div className={s.smallTitle}>Step 1: Screening resume</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button className={s.newVacancyButton}> + New Vacancy</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
